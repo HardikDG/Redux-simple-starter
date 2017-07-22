e79eb001a69a35287b20f6fa47ea51f7dc20e664
HardikDG <hardikdg.sa@gmail.com>
2017-07-22 12:39:56 +0530

    Added book details selection

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 import BookList from '../containters/book-list';
+import BookDetail from '../containters/book-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList/>
+        <BookDetail />
       </div>
     );
   }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
 export default function () {
     return [
-        { title: "Javascript : good parts" },
-        { title: "Harry Potter" },
-        { title: "big nerd ranch android" },
+        { title: "Javascript : good parts", pages: 34 },
+        { title: "Harry Potter", pages: 45 },
+        { title: "big nerd ranch android", pages: 56 },
     ]
 }
